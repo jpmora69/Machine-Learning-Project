c6985510544cd9cba407bce6afff7974e78cfc5b
Elixeus <eliseowang@gmail.com>
2016-04-14 00:46:33 -0400

    geocoding function

---
+++ supportingFunctions/geocoding.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import pandas as pd
+from geopy.geocoders import Nominatim, GoogleV3, GeoNames
+
+
+def geocoding(fname, coder=GeoNames, address='Address',
+              n_init=0, n_end=5):
+    '''
+    This function takes 5 arguments: fname, coder, col, n_init, n_end
+    fname: the pandas dataframe you want to geocode
+    coder: geocoder of your choice: e.g. Nominatim, GoogleV3, GeoNames
+    address: the column containing the address you want to geocode
+    n_init: the starting index
+    n_end: the finishing index
+    '''
+    # prepare the table: create two new columns: 'lat' and 'lon'
+    fname['lat'] = None
+    fname['lon'] = None
+    try:
+        fname['lat'] = fname.loc[n_init: n_end,
+                                 address].apply(lambda x:
+                                                coder.geocode(x).latitude)
+        fname['lon'] = fname.loc[n_init: n_end,
+                                 address].apply(lambda x:
+                                                coder.geocode(x).longitude)
