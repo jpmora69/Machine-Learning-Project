ff988b0010ef56b1d058cb911b8f63ebaf14027c
Elixeus <eliseowang@gmail.com>
2016-04-14 22:20:26 -0400

    change the structure of the script

--- GeocodeRestaurants.py
+++ GeocodeRestaurants.py
@@ -4,7 +4,10 @@ import geopy
 from supportFunctions.GEOCODING import geocoding
 
 
-if __name__ == '__main__':
+def main():
     data = pd.read_csv('restaurants_inspection.csv')
     geocoding(data, coder=geopy.geocoders.Nominatim(),
               address='Address', n_init=0, n_end=-1)
+
+if __name__ == '__main__':
+    main()
