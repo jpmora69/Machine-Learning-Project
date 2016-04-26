22ce314f9ca4c691ef08c4a7cef5c852094326f4
Juan Mora <jml1023@nyu.edu>
2016-04-26 10:48:17 -0400

    final version of the RNN and clustering code

--- Machine Learning project.py
+++ Machine Learning project.py
@@ -1,7 +1,6 @@
 
 # coding: utf-8
-#Created by Juan Pablo
-
+#Author @juan pablo
 
 import pandas as pd
 import numpy as np
@@ -15,7 +14,6 @@ from sklearn import preprocessing
 from matplotlib.colors import ListedColormap
 get_ipython().magic(u'matplotlib inline')
 from rnn import RNN, MetaRNN
-
 
 food = pd.read_csv('food_final.csv')
 food = food[food.SCORE > 0]
@@ -93,13 +91,12 @@ plt.xlim([-74.3, -73.6])
 plt.grid()
 
 
-food_sel_scaled_df = pd.DataFrame(preprocessing.scale(food_sel), 
-                                  columns =['rating','SCORE','price.tier', 'stats.checkinsCount'])
+food_sel_scaled_df = pd.DataFrame(preprocessing.scale(food_sel), columns =['rating','SCORE','price.tier', 'stats.checkinsCount'])
 
 
 #Dividing the sample into train and test sets
 np.random.seed(2015)
-ind=spst.bernoulli.rvs(p = 0.6, size = len(food_sel1))
+ind=spst.bernoulli.rvs(p = 0.75, size = len(food_sel1))
 food_sel_scaled_train = food_sel_scaled_df[ind==1]
 food_sel_scaled_test = food_sel_scaled_df[ind==0] 
 
@@ -108,7 +105,7 @@ n_hidden = 5 # M
 n_in = 3      # D
 n_out = 1     # K
 n_steps = 1  # the length of each sequence
-n_seq = len(food_sel_train)   # the number of datapoints (i.e. sequences)
+n_seq = len(food_sel_scaled_train)   # the number of datapoints (i.e. sequences)
 
 #Creating input and output arrays for training RNN         
 rating = np.array(food_sel_scaled_train[['rating','price.tier','stats.checkinsCount']]).reshape(n_seq,n_steps,n_in)
@@ -120,15 +117,42 @@ model = MetaRNN(n_in=n_in, n_hidden=n_hidden, n_out=n_out,
                     n_epochs=50, activation='tanh')
 model.fit(rating, score, validation_frequency=5000)
 
-
 guess = model.predict(rating.reshape(len(rating), n_in))
 scores_pred = pd.DataFrame(guess)
 
 scores_pred.columns = ['predictions']
+
 scores_pred.predictions.plot(kind='hist', bins=20, figsize=(6,4), grid=True,                             title = "Histogram of predicted DOH normalized Scores (IS)", alpha=0.8)
+
 food_sel_scaled_train.SCORE.plot(kind='hist', bins=20, figsize=(6,4), grid=True,                             title = "Histogram of actual DOH normalized Scores (IS)", alpha=0.8)
 
+#Creating parameters for training RNN
+n_hidden = 5 # M
+n_in = 3      # D
+n_out = 1     # K
+n_steps = 1  # the length of each sequence
+n_seq = len(food_sel_scaled_test)   # the number of datapoints (i.e. sequences)
+
+#Creating input and output arrays for training RNN         
+rating1 = np.array(food_sel_scaled_test[['rating','price.tier','stats.checkinsCount']]).reshape(n_seq,n_steps,n_in)
+score1 = np.array(food_sel_scaled_test['SCORE']).reshape(n_seq,n_steps,n_out)
+
+guess1 = model.predict(rating1.reshape(len(rating1), n_in))
+scores_pred1 = pd.DataFrame(guess1)
+
+scores_pred1.columns = ['predictions']
+
+scores_pred1.predictions.plot(kind='hist', bins=20, figsize=(6,4), grid=True,                             title = "Histogram of predicted DOH normalized Scores (OS)", alpha=0.8)
+
+food_sel_scaled_test.SCORE.plot(kind='hist', bins=20, figsize=(6,4), grid=True,                             title = "Histogram of actual DOH normalized Scores (OS)", alpha=0.8)
+
+fig = plt.figure(figsize=(8,6))
+ax = plt.scatter(x=food_sel_scaled_test.SCORE, y=scores_pred1.predictions,            alpha=0.8)
+plt.grid()
+plt.title('OS predicted and actual results (Normalized)')
+
 BCsubset = (food[food.SCORE > 13])
+
 latlon1 = pd.DataFrame(list(BCsubset.latlon.str.split()))
 latlon1.columns = ['latitude', 'longitude']
 latlon1['latitude'] = latlon1['latitude'].map(lambda x: str(x)[:-1])
@@ -144,7 +168,6 @@ plt.ylim([40.45, 40.95])
 plt.xlim([-74.3, -73.6])
 plt.grid()
 
-
 ax3 = BCsubset['SCORE'].plot(kind='hist', bins = 20, figsize=(6,4), grid=True, 
                 title="Histogram of DOH Scores (B and C)", alpha=0.8)
 ax = BCsubset.plot(x='rating', y='SCORE', kind='scatter', figsize=(8,6), grid=True,
@@ -157,7 +180,6 @@ ax1.set(xlabel="Price Tier", ylabel="DOH Score")
 
 ax2 = food['rating'].plot(kind='hist', bins = 20, figsize=(6,4), grid=True,
                  title="Histogram of 4Square ratings (B & C restaurants)", alpha=0.8)
-
 
 #Subsetting and whitening the data for clustering
 BC_food_sel = BCsubset[['rating','SCORE','price.tier', 'stats.checkinsCount']]
@@ -174,8 +196,7 @@ colors = est.labels_.astype(np.float)
 cmap = ListedColormap(['blue','green','yellow','red', 'cyan'])
 plt.figure(figsize=(10, 8))
 boros.plot(alpha=0)
-plt.scatter(latlon1.longitude, latlon1.latitude, s=20, c=colors, 
-            cmap=cmap, alpha = 0.5)
+plt.scatter(latlon1.longitude, latlon1.latitude, s=20, c=colors, cmap=cmap,             alpha = 0.5)
 plt.title("Clustering with K-means and 2 clusters", fontsize = 20)
 plt.xlabel('Latitude', fontsize=16)
 plt.ylabel('Longitude', fontsize=16)
@@ -201,12 +222,11 @@ plt.ylim([40.5, 40.95])
 plt.xlim([-74.3, -73.6])
 plt.grid()
 
-BC_food_sel_scaled_df = pd.DataFrame(preprocessing.scale(BC_food_sel), 
-                                     columns =['rating','SCORE','price.tier',                                                                                 'stats.checkinsCount'])
+BC_food_sel_scaled_df = pd.DataFrame(preprocessing.scale(BC_food_sel), columns =['rating','SCORE','price.tier',                                                                                 'stats.checkinsCount'])
 
 #Dividing the sample into train and test sets
 np.random.seed(2015)
-ind=spst.bernoulli.rvs(p = 0.6, size = len(BC_food_sel))
+ind=spst.bernoulli.rvs(p = 0.75, size = len(BC_food_sel))
 BC_food_sel_scaled_train = BC_food_sel_scaled_df[ind==1]
 BC_food_sel_scaled_test = BC_food_sel_scaled_df[ind==0] 
 
@@ -224,7 +244,7 @@ BC_score = np.array(BC_food_sel_scaled_train['SCORE']).reshape(n_seq,n_steps,n_o
 #Creating the model and feeding it with training data set
 model = MetaRNN(n_in=n_in, n_hidden=n_hidden, n_out=n_out,
                     learning_rate=0.001, learning_rate_decay=0.999,
-                    n_epochs=500, activation='tanh')
+                    n_epochs=200, activation='tanh')
 model.fit(BC_rating, BC_score, validation_frequency=5000)
 
 BCguess = model.predict(BC_rating.reshape(len(BC_rating), n_in))
@@ -234,9 +254,32 @@ BCscores_pred.columns = ['predictions']
 
 BCscores_pred.predictions.plot(kind='hist', bins=20, figsize=(6,4), grid=True,                             title = "Histogram of predicted DOH normalized Scores B&C (IS)", alpha=0.8)
 
-BC_food_sel_scaled_train.SCORE.plot(kind='hist', bins=20, figsize=(6,4), 
-                                    grid=True, 
-                                    title = "Histogram of actual DOH normalized Scores B&C (IS)", alpha=0.8)
+BC_food_sel_scaled_train.SCORE.plot(kind='hist', bins=20, figsize=(6,4), grid=True,                             title = "Histogram of actual DOH normalized Scores B&C (IS)", alpha=0.8)
+
+#Creating parameters for training RNN
+n_hidden = 5 # M
+n_in = 3      # D
+n_out = 1     # K
+n_steps = 1  # the length of each sequence
+n_seq = len(BC_food_sel_scaled_test)   # the number of datapoints (i.e. sequences)
+
+#Creating input and output arrays for training RNN         
+BC_rating1 = np.array(BC_food_sel_scaled_test[['rating','price.tier','stats.checkinsCount']]).reshape(n_seq,n_steps,n_in)
+BC_score1 = np.array(BC_food_sel_scaled_test['SCORE']).reshape(n_seq,n_steps,n_out)
+
+BCguess1 = model.predict(BC_rating1.reshape(len(BC_rating1), n_in))
+BCscores_pred1 = pd.DataFrame(BCguess1)
+
+BCscores_pred1.columns = ['predictions']
+
+BCscores_pred1.predictions.plot(kind='hist', bins=20, figsize=(6,4), grid=True,                             title = "Histogram of predicted DOH normalized Scores B&C (OS)", alpha=0.8)
+
+BC_food_sel_scaled_test.SCORE.plot(kind='hist', bins=20, figsize=(6,4), grid=True,                             title = "Histogram of actual DOH normalized Scores B&C (OS)", alpha=0.8)
+
+fig = plt.figure(figsize=(8,6))
+ax = plt.scatter(x=BC_food_sel_scaled_test.SCORE, y=BCscores_pred1.predictions,            alpha=0.8)
+plt.grid()
+plt.title('OS predicted and actual results (Normalized)')
 
 
 
